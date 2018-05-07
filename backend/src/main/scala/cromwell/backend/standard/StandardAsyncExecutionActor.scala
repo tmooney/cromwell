9ae07250f973a13beaa04eb45ad48c48524f5195
Miguel Covarrubias <mcovarr@broadinstitute.org>
2018-05-07 13:32:29 -0400

    um seriously?

--- backend/src/main/scala/cromwell/backend/standard/StandardAsyncExecutionActor.scala
+++ backend/src/main/scala/cromwell/backend/standard/StandardAsyncExecutionActor.scala
@@ -316,6 +316,7 @@ trait StandardAsyncExecutionActor extends AsyncBackendJobExecutionActor with Sta
     s"""|#!$jobShell
         |DOCKER_OUTPUT_DIR_LINK
         |cd $callDirectory
+        |mkdir execution
         |tmpDir=`$temporaryDirectory`
         |chmod 777 "$$tmpDir"
         |export _JAVA_OPTIONS=-Djava.io.tmpdir="$$tmpDir"
